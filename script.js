0ae02edc721789a1969fb1bb67e63787e849eb65
qwe-kev <kevray39@gmail.com>
2022-11-28 01:35:48 +0530

    DOM task 3

---
+++ script.js
@@ -0,0 +1,6 @@
+var headerTitle = document.getElementById("main-header");
+headerTitle.style.borderBottom = "2px solid #000";
+
+var headerClasses = document.getElementsByClassName("title");
+headerClasses[0].setAttribute("style","font-weight:bold");
+headerClasses[0].setAttribute("style","color:green");
