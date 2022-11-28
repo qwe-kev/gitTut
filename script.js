06e17d4a6b4dd7189c8a2d5d570248490f06f607
qwe-kev <kevray39@gmail.com>
2022-11-28 23:34:11 +0530

    DOM task 5

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-var headerTitle = document.getElementById("main-header");
-headerTitle.style.borderBottom = "2px solid #000";
+const node = document.createElement("li");
+const textNode = document.createTextNode("Item 5");
+node.appendChild(textNode);
 
-var headerClasses = document.getElementsByClassName("title");
-headerClasses[0].setAttribute("style","font-weight:bold");
-headerClasses[0].setAttribute("style","color:green");
+var listGroup = document.getElementById("items").appendChild(node);
 
-var itemsList = document.getElementsByClassName("list-group-item");
-itemsList[2].style.backgroundColor = "green";
-
-for(let i = 0;i < itemsList.length;i++) {
-    itemsList[i].style.fontWeight = "bold";
-}
+var listItem = document.getElementById("items").lastElementChild;
+listItem.style.fontWeight = "bold";
