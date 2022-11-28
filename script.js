d602f6154d4b6d8ca1ea79d0c9cb8a0d4ee0d550
qwe-kev <kevray39@gmail.com>
2022-11-28 10:59:52 +0530

    task-4

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ headerTitle.style.borderBottom = "2px solid #000";
 var headerClasses = document.getElementsByClassName("title");
 headerClasses[0].setAttribute("style","font-weight:bold");
 headerClasses[0].setAttribute("style","color:green");
+
+var itemsList = document.getElementsByClassName("list-group-item");
+itemsList[2].style.backgroundColor = "green";
+
+for(let i = 0;i < itemsList.length;i++) {
+    itemsList[i].style.fontWeight = "bold";
+}
